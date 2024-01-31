a0d78436c91ad3cd7b6330507283b52feaf0d38f
yuziwoo <yuziwoo@naver.com>
2024-02-01 00:55:21 +0900

    chore: MainLiner 제거

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,9 @@
 import Introduce from '../../components/Main/Introduce/Introduce';
-import MainLiner from '../../components/Main/MainLiner/MainLiner';
 
 const MainPage = () => {
   return (
     <div>
       <Introduce />
-      <MainLiner />
     </div>
   );
 };
